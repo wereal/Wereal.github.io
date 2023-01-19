5d2ee7b8367767ac5cefd87fca7c905f0742ac93
Marios Iosifidis <94121668+wereal@users.noreply.github.com>
2023-01-19 19:23:08 +0200

    19.1.2023
    Home Button fixed
    when press wereal logo redirect to home page from every page

--- jselements/homelink_function.js
+++ jselements/homelink_function.js
@@ -1,5 +1,3 @@
-var homeLink = document.getElementById("home-link");
-homeLink.addEventListener("click", function() {
-  window.location = "index.html";
+document.querySelector(".logo").addEventListener("click", function() {
+  window.open("../index.html", "_blank");
 });
-
