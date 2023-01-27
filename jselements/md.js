36aa1c257f39e26a1c772739519c3dede46ee44e
Marios Iosifidis <94121668+wereal@users.noreply.github.com>
2023-01-27 23:05:52 +0200

    27.1.20.23
    27.1.20.23

---
+++ jselements/md.js
@@ -0,0 +1,7 @@
+// check the device width 
+if(screen.width <= 767){
+    var element = document.querySelector(".typewriter");
+    element.style.backgroundColor = "initial";
+    element.style.transition = "none";
+  }
+  
